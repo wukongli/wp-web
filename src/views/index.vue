9d9a1d8b28e568efe089cbf9f146be0afee6899b
lidongqiha <lidongqi.ha@chinatelecom.cn>
2024-03-15 15:17:50 +0800

    初始化项目

---
+++ src/views/index.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="app-container home">
+    <iframe
+      width="100%"
+      height="100%"
+      src="http://1.94.42.208:8080/#/index"
+      title="网盘解析"
+    ></iframe>
+  </div>
+</template>
+
+<script setup name="Index">
+const version = ref('3.8.5');
+
+function goTarget(url) {
+  window.open(url, '__blank');
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  height: calc(100vh - 84px);
+  position: relative;
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: 'open sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,234 @@
+<template>
+  <div class="login">
+    <el-form
+      ref="loginRef"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+    >
+      <h3 class="title">网盘高速下载平台</h3>
+      <el-form-item prop="username">
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          size="large"
+          auto-complete="off"
+          placeholder="请输入激活码"
+        >
+          <template #prefix
+            ><svg-icon icon-class="user" class="el-input__icon input-icon"
+          /></template>
+        </el-input>
+      </el-form-item>
+      <!-- <el-form-item prop="password">
+        <el-input
+          v-model="loginForm.password"
+          type="password"
+          size="large"
+          auto-complete="off"
+          placeholder="密码"
+          @keyup.enter="handleLogin"
+        >
+          <template #prefix
+            ><svg-icon icon-class="password" class="el-input__icon input-icon"
+          /></template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="code" v-if="captchaEnabled">
+        <el-input
+          v-model="loginForm.code"
+          size="large"
+          auto-complete="off"
+          placeholder="验证码"
+          style="width: 63%"
+          @keyup.enter="handleLogin"
+        >
+          <template #prefix
+            ><svg-icon icon-class="validCode" class="el-input__icon input-icon"
+          /></template>
+        </el-input>
+        <div class="login-code">
+          <img :src="codeUrl" @click="getCode" class="login-code-img" />
+        </div>
+      </el-form-item>
+      <el-checkbox
+        v-model="loginForm.rememberMe"
+        style="margin: 0px 0px 25px 0px"
+        >记住密码</el-checkbox
+      > -->
+      <el-form-item style="width: 100%">
+        <el-button
+          :loading="loading"
+          size="large"
+          type="primary"
+          style="width: 100%"
+          @click.prevent="handleLogin"
+        >
+          <span v-if="!loading">激 活</span>
+          <span v-else>登 录 中...</span>
+        </el-button>
+        <div style="float: right" v-if="register">
+          <router-link class="link-type" :to="'/register'"
+            >立即注册</router-link
+          >
+        </div>
+      </el-form-item>
+    </el-form>
+    <!--  底部  -->
+    <div class="el-login-footer">
+      <span>Copyright 2024 All Rights Reserved.</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { getCodeImg, querySecretKey } from '@/api/login';
+import Cookies from 'js-cookie';
+import { encrypt, decrypt } from '@/utils/jsencrypt';
+import useUserStore from '@/store/modules/user';
+
+const userStore = useUserStore();
+const router = useRouter();
+const { proxy } = getCurrentInstance();
+
+const loginForm = ref({
+  username: '',
+  password: '',
+  rememberMe: false,
+  code: '',
+  uuid: '',
+});
+
+const loginRules = {
+  username: [{ required: true, trigger: 'blur', message: '请输入激活码' }],
+  // password: [{ required: true, trigger: 'blur', message: '请输入您的密码' }],
+  // code: [{ required: true, trigger: 'change', message: '请输入验证码' }],
+};
+
+const codeUrl = ref('');
+const loading = ref(false);
+// 验证码开关
+const captchaEnabled = ref(true);
+// 注册开关
+const register = ref(false);
+const redirect = ref(undefined);
+
+function handleLogin() {
+  proxy.$refs.loginRef.validate((valid) => {
+    if (valid) {
+      loading.value = true;
+      // 勾选了需要记住密码设置在 cookie 中设置记住用户名和密码
+      // if (loginForm.value.rememberMe) {
+      //   Cookies.set('username', loginForm.value.username, { expires: 30 });
+      //   Cookies.set('password', loginForm.value.password, {
+      //     expires: 30,
+      //   });
+      //   Cookies.set('rememberMe', loginForm.value.rememberMe, { expires: 30 });
+      // } else {
+      //   // 否则移除
+      //   Cookies.remove('username');
+      //   Cookies.remove('password');
+      //   Cookies.remove('rememberMe');
+      // }
+      // 调用action的登录方法
+      loginForm.value.password = 123456;
+      userStore
+        .login(loginForm.value)
+        .then(() => {
+          router.push({ path: redirect.value || '/' });
+        })
+        .catch(() => {
+          loading.value = false;
+          // 重新获取验证码
+          // if (captchaEnabled.value) {
+          //   getCode();
+          // }
+        });
+    }
+  });
+}
+
+// function getCode() {
+//   getCodeImg().then((res) => {
+//     captchaEnabled.value =
+//       res.captchaEnabled === undefined ? true : res.captchaEnabled;
+//     if (captchaEnabled.value) {
+//       codeUrl.value = 'data:image/gif;base64,' + res.img;
+//       loginForm.value.uuid = res.uuid;
+//     }
+//   });
+// }
+
+// function getSecretKey() {
+//     Cookies.set('publicKey', 'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALBG1hY69Hu9s1pnDOBr+6ASbfaUPxV2PX6Xgnd/4Juud+f90qOQ4/ywBqJKYcZSaLx+3woVY75ynFFv1sfAzo8CAwEAAQ==');
+//     Cookies.set('privateKey', 'MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAsEbWFjr0e72zWmcM%0A4Gv7oBJt9pQ/FXY9fpeCd3/gm6535/3So5Dj/LAGokphxlJovH7fChVjvnKcUW/W%0Ax8DOjwIDAQABAkBX+yMDeW1pDWelKWXt5Tdzz37+4UMTLS7ILkq55iNuz7Nfh+7D%0A7EPREPIPgWUTuBDUZLXTWh3Jbg7oAvpOQAkhAiEA3YM42G1JbivBREPKp7jJvvw9%0Ax5bJhNQ/fYXbE2vzrEkCIQDLuKhgqP1gxLu0+4f4n6iXHuTlPGsezA3FMtfJF/G0%0AFwIgVECX+4G930CXNv7N8vNPEOxiFyscJQCR0Y17ISz7NrkCIQCoNO/R37ZWEBps%0AdMLwJeOt43RbUmegJhu4lyJUh9CqQQIgcjxOCXVj7EkTw7RmjKuyDZrYP2f307Fk%0ACb3NkEwaljk=');
+// }
+//
+// getSecretKey();
+
+// getCode();
+</script>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-image: url('../assets/images/bac.jpg');
+  background-size: cover;
+}
+.title {
+  margin: 0px auto 30px auto;
+  text-align: center;
+  color: #707070;
+}
+
+.login-form {
+  border-radius: 6px;
+  background: #ffffff;
+  width: 400px;
+  padding: 25px 25px 5px 25px;
+  .el-input {
+    height: 40px;
+    input {
+      height: 40px;
+    }
+  }
+  .input-icon {
+    height: 39px;
+    width: 14px;
+    margin-left: 0px;
+  }
+}
+.login-tip {
+  font-size: 13px;
+  text-align: center;
+  color: #bfbfbf;
+}
+.login-code {
+  width: 33%;
+  height: 40px;
+  float: right;
+  img {
+    cursor: pointer;
+    vertical-align: middle;
+  }
+}
+.el-login-footer {
+  height: 40px;
+  line-height: 40px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+  font-family: Arial;
+  font-size: 12px;
+  letter-spacing: 1px;
+}
+.login-code-img {
+  height: 40px;
+  padding-left: 12px;
+}
+</style>
