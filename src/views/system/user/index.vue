8f5a3bb451d3bca8fb903841124e67f040341c9e
wukongli <254750646@qq.com>
2024-10-25 11:39:17 +0800

    feat:更新

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -362,7 +362,7 @@ import moment from 'moment';
 const router = useRouter();
 const { proxy } = getCurrentInstance();
 const { sys_normal_disable, sys_user_sex } = proxy.useDict("sys_normal_disable", "sys_user_sex");
-
+import store from "@/store/index";
 const userList = ref([]);
 const open = ref(false);
 const loading = ref(true);
@@ -586,7 +586,14 @@ function handleAdd() {
   reset();
   getUser().then(response => {
     postOptions.value = response.posts;
-    roleOptions.value = response.roles;
+    const username = store.state.value.user.name;
+    if(username === "admin"){
+      roleOptions.value = response.roles;
+    }else{
+      roleOptions.value = response.roles.filter(e=>{
+        return e.roleName !== "管理员";
+      });
+    }
     open.value = true;
     title.value = "添加用户";
     form.value.password = initPassword.value;
@@ -602,7 +609,14 @@ function handleUpdate(row) {
     }
     form.value = response.data;
     postOptions.value = response.posts;
-    roleOptions.value = response.roles;
+    const username = store.state.value.user.name;
+    if(username === "admin"){
+      roleOptions.value = response.roles;
+    }else{
+      roleOptions.value = response.roles.filter(e=>{
+        return e.roleName !== "管理员";
+      });
+    }
     form.value.postIds = response.postIds;
     form.value.roleIds = response.roleIds;
     open.value = true;
