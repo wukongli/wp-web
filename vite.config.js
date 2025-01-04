9069f0af94b69d93437999cfb24e7c720c20d6d4
wukongli <254750646@qq.com>
2025-01-04 17:45:32 +0800

    feat:更新

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig(({ mode, command }) => {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
           // target: 'http://127.0.0.1:8081',
-          target: 'https://aifenxiang.net.cn:8081',
+          target: 'https://api.aifenxiang.net.cn',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, ''),
         },
