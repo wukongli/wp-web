8d291dfa06d66502d7155b77b63fa3a34d69b030
wukongli <254750646@qq.com>
2025-07-10 15:10:39 +0800

    feat:更新

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 <script setup>
 import useSettingsStore from '@/store/modules/settings';
 import { handleThemeStyle } from '@/utils/theme';
-
+import DisableDevtool from 'disable-devtool';
+DisableDevtool();
 onMounted(() => {
   nextTick(() => {
     // 初始化主题样式
