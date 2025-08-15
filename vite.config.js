fa4e8f7eee42fc4aae50ced5704970e76e35176c
wukongli <254750646@qq.com>
2025-08-15 10:10:21 +0800

    feat:更新

--- vite.config.js
+++ vite.config.js
@@ -35,8 +35,8 @@ export default defineConfig(({ mode, command }) => {
         '/dev-api': {
           // target: 'http://127.0.0.1:8081',
           // target: 'https://aifenxiang.net.cn:8081',
-          // target: 'https://api.gssource.com/',
-          target: 'https://api.aifenxiang.net.cn/',
+          target: 'https://api.gssource.com/',
+          // target: 'https://api.aifenxiang.net.cn/',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, ''),
         },
