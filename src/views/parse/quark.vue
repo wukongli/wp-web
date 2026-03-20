c87d9f10601dedaf52f45fc6eb883dc3435ffc99
wukongli <254750646@qq.com>
2026-03-20 21:19:33 +0800

    feat:更新

--- src/views/parse/quark.vue
+++ src/views/parse/quark.vue
@@ -871,7 +871,7 @@ async function confirm(item) {
           item.loading = false;
           item.disable = false;
           res.data.data.data.forEach((data)=>{
-            loadData.isMobile ? sendToGopeed(data,):sendToMotrix(data);
+            loadData.isMobile ? sendToGopeed(data,res.data.id):sendToMotrix(data,res.data.id);
             item.status = 2;
           })
         } else {
@@ -933,7 +933,7 @@ async function sendToGopeed(data,id) {
     },
     body: JSON.stringify({req:
           {
-            url:loadData.url,
+            url:data.download_url,
             extra:{
               header:{
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) quark-cloud-drive/3.0.2 Chrome/100.0.4896.160 Electron/18.3.5.12-a038f7b798 Safari/537.36 Channel/pckk_clouddrive_share_ch",
