640273c5968e661a1b57244a1d42d7276312561c
wukongli <254750646@qq.com>
2026-06-21 22:43:10 +0800

    feat:更新

--- src/views/parse/quark.vue
+++ src/views/parse/quark.vue
@@ -816,8 +816,10 @@ async function sendToMotrix(data,id) {
             `User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) quark-cloud-drive/3.0.2 Chrome/100.0.4896.160 Electron/18.3.5.12-a038f7b798 Safari/537.36 Channel/pckk_clouddrive_share_ch`,
             `Cookie: ${id}` // Cookie 必须包含在 header 里
           ],
-          'max-connection-per-server': '256',
-          'split': '256',
+          'max-connection-per-server': '256',    // aria2 硬上限，写 255 无效！
+          'split': '256',                        // 分块数拉到 Motrix 上限
+          'min-split-size': '1M',              // 每块至少 1MB，减少碎片
+          'disk-cache': '512M'                 // 务必加这条，避免磁盘卡顿
         }
       ]
     })
