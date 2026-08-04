a3725e54268d213bbe05ed1fa8609fb5c669629b
wukongli <254750646@qq.com>
2026-08-04 15:59:07 +0800

    更新

--- src/views/parse/quark.vue
+++ src/views/parse/quark.vue
@@ -1177,6 +1177,8 @@ async function initToken() {
             fid: data.data.data.list[0].fid,
           };
           parseList(params);
+        } else {
+          loadData.tableLoading = false;
         }
       } else {
         ElMessage.error('文件资源已失效');
